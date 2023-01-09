b58b889fb708e4a2f1b98a712468c5bb87e96e62
OutM1ckey <83223712+OutM1ckey@users.noreply.github.com>
2023-01-09 14:43:53 +0100

    poprawki

--- Client.cpp
+++ Client.cpp
@@ -1,28 +1,28 @@
-﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS //ubsługa warningów winsoc
+#define _CRT_SECURE_NO_WARNINGS //ubsługa warningów pliki
+#include <stdio.h> 
 #include <winsock2.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#define SERVER "127.0.0.1"
-#define PORT 8888
+#define SERVER "127.0.0.1" //local
+#define PORT 8888 //obojetnie ktory
 
-void ShowCerts(SSL* ssl)
+void ShowCerts(SSL* ssl) //wyswietlanie certyfikatów
 {
 	X509* cert;
 	char* line;
-	cert = SSL_get_peer_certificate(ssl); /* get the server's certificate */
+	cert = SSL_get_peer_certificate(ssl); // prosi o komunikat z certyfikatem serweru 
 	if (cert != NULL)
 	{
 		printf("Server certificates:\n");
-		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0); //subject
 		printf("Subject: %s\n", line);
 		free(line);       /* free the malloc'ed string */
 		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
 		printf("Issuer: %s\n", line);
 		free(line);       /* free the malloc'ed string */
-		X509_free(cert);     /* free the malloc'ed certificate copy */
+		X509_free(cert);     // czysci pamiec certyfikatu
 	}
 	else
 		printf("Info: No client certificates configured.\n");
@@ -57,7 +57,7 @@ int main()
 	// Struktura z informacjami o adresie serwera
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
 	server_addr.sin_port = htons(PORT);
 
 	// Łączenie z serwerem
@@ -69,13 +69,13 @@ int main()
 		return 1;
 	}	
 	
-	SSL_CTX* ssl_ctx = SSL_CTX_new(TLS_client_method());
+	SSL_CTX* ssl_ctx = SSL_CTX_new(TLS_client_method()); //tworzenie contextu ssl, metoda tls
 
 	if (!ssl_ctx)
 	{
 		printf("SSL_CTX_new failed.\n");
 		closesocket(sock);
-		WSACleanup();
+		WSACleanup(); //czyszczenie
 		return 1;
 	}
 
@@ -93,6 +93,7 @@ int main()
 		return 1;
 	}
 
+	//sprawdzenie czy pasuje/jest odpowiedni klucz do certyfikatu
 	if (!SSL_CTX_check_private_key(ssl_ctx)) {
 		printf("Private key does not match the certificate");
 		return 1;
@@ -108,7 +109,7 @@ int main()
 	// Przypisanie gniazda do struktury SSL
 	SSL_set_fd(ssl, sock);
 	int ret;
-	// Nawiązywanie połączenia SSL
+	// Nawiązywanie połączenia SSL (w sesji)
 	if (ret = SSL_connect(ssl) != 1)
 	{
 		printf("SSL_connect failed.Error %d\n",SSL_get_error(ssl,ret));
@@ -119,7 +120,7 @@ int main()
 		return 1;
 	}
 
-	ShowCerts(ssl);
+	ShowCerts(ssl); // wypisywanie certyfikatu
 
 	// Wysyłanie certyfikatu klienta
 	char buffer[1025];
@@ -139,9 +140,9 @@ int main()
 	{
 		// Wczytywanie komendy od użytkownika
 		printf("Enter command: ");
-		fgets(buffer, sizeof(buffer), stdin); 
+		fgets(buffer, sizeof(buffer), stdin); //wczytywanie z klawiatury
 		
-		if (buffer[0] == '\n')
+		if (buffer[0] == '\n') //wczytywanie enter/ wychodz z petli
 			break;
 
 		// Obsługa komendy 'send'
@@ -151,6 +152,7 @@ int main()
 			if (file == NULL) {
 				perror("Nie udało się otworzyć pliku");
 				exit(1);
+				//break;
 			}
 
 			// Wysyłanie komendy do serwera
@@ -161,14 +163,14 @@ int main()
 			}
 			int n;
 			// Pętla wysyłająca plik po 1024 bajty
-			while ((n = fread(buffer, 1, 1024, file)) > 0) {
-				int bytes_sent = SSL_write(ssl, buffer, n);
-				if (bytes_sent < 0) {
+			while ((n = fread(buffer, 1, 1024, file)) > 0) { //zczytuje dane z pliku
+				int bytes_sent = SSL_write(ssl, buffer, n); //wysyła plik na serwer
+				if (bytes_sent < 0) { //czy bład podczas wysyłania
 					perror("Błąd przy wysyłaniu pliku");
 					exit(1);
 				}
 			}
-			SSL_write(ssl, "END_OF_FILE", 11);
+			SSL_write(ssl, "END_OF_FILE", 11); //informacja o końcu pliku
 		}
 		// Obsługa komendy 'read'
 		else if (strncmp(buffer, "read", 4) == 0)
@@ -187,12 +189,12 @@ int main()
 					break;
 				}
 				// Sprawdzanie, czy otrzymano znak konca wysylania danych
-				if (strncmp(buffer, "END_OF_FILE", 11) == 0)
+				if (strncmp(buffer, "END_OF_FILE", 11) == 0) //sprawdza zakonczenie przesylania
 				{
 					break;
 				}
-				buffer[size] = '\0';
-				printf("%s", buffer);
+				buffer[size] = '\0'; //znak konca stringu
+				printf("%s", buffer); // wypisywanie na ekranie
 			}
 			printf("\n");
 			
@@ -210,117 +212,4 @@ int main()
 	WSACleanup();
 
 	return 0;
-
-}
-
-int main2()
-{
-	// Inicjalizacja Winsock
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("WSAStartup failed.\n");
-		return 1;
-	}
-
-	// Utworzenie gniazda
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET)
-	{
-		printf("Could not create socket.\n");
-		return 1;
-	}
-
-	// Przygotowanie struktury adresowej
-	struct sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(PORT);
-	server.sin_addr.s_addr = inet_addr(SERVER);
-
-	// Łączenie się z serwerem
-	if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0)
-	{
-		printf("Connect failed.\n");
-		return 1;
-	}
-
-	// Inicjalizacja OpenSSL
-	SSL_library_init();
-	SSL_load_error_strings();
-	SSL_CTX* ctx = SSL_CTX_new(SSLv23_client_method());
-
-	// Wczytanie certyfikatu
-	if (!SSL_CTX_use_certificate_file(ctx, "client_cert.pem", SSL_FILETYPE_PEM))
-	{
-		printf("Could not load certificate.\n");
-		return 1;
-	}
-
-	// Wczytanie klucza prywatnego
-	if (!SSL_CTX_use_PrivateKey_file(ctx, "client_key.pem", SSL_FILETYPE_PEM))
-	{
-		printf("Could not load private key.\n");
-		return 1;
-	}
-
-	// Tworzenie struktury SSL dla połączenia
-	SSL* ssl = SSL_new(ctx);
-	if (!ssl)
-	{
-		printf("SSL_new failed.\n");
-		return 1;
-	}
-
-	// Przypisanie gniazda do struktury SSL
-	SSL_set_fd(ssl, sock);
-
-	// Tworzenie struktury SSL dla połączenia
-	if (!ssl)
-	{
-		printf("SSL_new failed.\n");
-		return 1;
-	}
-
-	// Przypisanie gniazda do struktury SSL
-	SSL_set_fd(ssl, sock);
-
-	// Łączenie się z serwerem przez SSL
-	if (SSL_connect(ssl) <= 0)
-	{
-		printf("SSL_connect failed.\n");
-		return 1;
-	}
-	else
-	{
-		printf("Connected to %s:%d.\n", SERVER, PORT);
-
-		// Wysyłanie danych do serwera
-		char buffer[1024] = "Hello, Server!";
-		int ret = SSL_write(ssl, buffer, sizeof(buffer));
-		if (ret <= 0)
-		{
-			printf("SSL_write failed.\n");
-			return 1;
-		}
-
-		// Odbieranie danych od serwera
-		ret = SSL_read(ssl, buffer, sizeof(buffer));
-		if (ret <= 0)
-		{
-			printf("SSL_read failed.\n");
-			return 1;
-		}
-
-		printf("Received: %s\n", buffer);
-
-		// Zamykanie połączenia i zwalnianie zasobów
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-		closesocket(sock);
-
-		// Zakończenie pracy Winsock
-		WSACleanup();
-
-		return 0;
-	}
 }
